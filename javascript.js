ff82e2c6a0983db9f49cf83cb48e22be478167ae
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-01-27 03:45:40 +0000

    Created functions to generate and reset grid, drawing works but only black

--- javascript.js
+++ javascript.js
@@ -1,23 +1,74 @@
+// function hoverOut() {
+
+// }
+
+function hoverIn(e) {
+  let currentSquare = e.srcElement;
+  currentSquare.style.backgroundColor = "white";
+}
+
+function attachContainer() {
+  container.id = "container";
+  container.style.gridTemplateColumns = 'repeat(' + size + ', auto)';
+  container.style.gridTemplateRows = 'repeat(' + size + ', auto)';
+  body.appendChild(container);
+}
+
+function clearGrid() {
+  while (container.childElementCount > 0) {
+    container.removeChild(container.firstChild);
+  }
+  body.removeChild(container);
+}
+
+function setupEventListeners() {
+  let squares = container.childNodes;
+  squares.forEach(square => {
+    square.addEventListener('mouseenter', hoverIn);
+    //square.addEventListener('mouseleave', hoverOut)
+  });
+  
+}
+
+// Generate square grid of given size
 function generateGrid(size) {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
+      square.className = 'square';
       container.appendChild(square);
+      square = document.createElement('div');
     }
   }
 }
 
 // Specify size of grid
-const size = 16;
+let size = 16;
+
+// Retrieve reset button to set up event listener
+const resetBtn = document.querySelector('#reset');
 
 // Create overall container
 const container = document.createElement('div');
 
 // Create single square div
-const square = document.createElement('div');
+let square = document.createElement('div');
 
+// Need body element so that total container can be appended
 const body = document.querySelector('body');
 
 generateGrid(size);
-container.style.gridTemplateColumns = repeat(size, auto);
-container.style.gridTemplateRows = repeat(size, auto);
-body.appendChild(container);
+
+// Setup event listeners for each of containers child divs
+setupEventListeners();
+
+attachContainer();
+
+resetBtn.addEventListener('click', () => {
+  size = prompt('Specify desired size of grid (100 max)');
+  size = size > 100 ? 100 : size;
+  clearGrid();
+  generateGrid(size);
+  setupEventListeners();
+  attachContainer();
+});
+
